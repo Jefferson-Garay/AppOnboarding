0923c13dfb8b6f26201f7e4cbad846d9b0d68eba
Jefferson <Jefferson@JEFFFERSON>
2025-11-21 17:33:11 -0500

    funcionando las apis de todos los usuarios get, post, put.

--- app/src/main/java/dev/jeff/apponboarding/data/repository/UsuarioRepository.kt
+++ app/src/main/java/dev/jeff/apponboarding/data/repository/UsuarioRepository.kt
@@ -1,13 +1,43 @@
 package dev.jeff.apponboarding.data.repository
 
+import android.util.Log
 import dev.jeff.apponboarding.data.model.UsuarioModel
 import dev.jeff.apponboarding.data.remote.RetrofitInstance
+import dev.jeff.apponboarding.data.remote.usuario.LoginRequest
+import retrofit2.HttpException
 
 class UsuarioRepository {
 
     suspend fun login(correo: String, password: String): UsuarioModel? {
-        val usuarios = RetrofitInstance.api.getUsuarios()
+        return try {
+            val response = RetrofitInstance.api.login(
+                LoginRequest(correo, password)
+            )
+            response.usuario
+        } catch (e: HttpException) {
+            Log.e("LOGIN", "Error HTTP: ${e.code()} - ${e.message()}")
+            null
+        } catch (e: Exception) {
+            Log.e("LOGIN", "Error inesperado: ${e.message}")
+            null
+        }
+    }
 
-        return usuarios.find { it.correo == correo && it.passwordHash == password }
+    suspend fun getUsuarios(): List<UsuarioModel> {
+        return try {
+            RetrofitInstance.api.getUsuarios()
+        } catch (e: Exception) {
+            Log.e("USUARIOS", "Error obteniendo usuarios: ${e.message}")
+            emptyList()
+        }
+    }
+
+    suspend fun crearUsuario(data: Map<String, Any>): String {
+        return try {
+            val res = RetrofitInstance.api.createUsuario(data)
+            res["message"] ?: "Usuario creado"
+        } catch (e: Exception) {
+            "Error creando usuario"
+        }
     }
 }
